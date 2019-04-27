0e114a27bec2488990fa2f9f09b18626a01e2437
justynias <justynakozik1@gmail.com>
2019-04-27 18:16:27 +0200

    initial custom sketch view

---
+++ app/src/main/java/com/just/sketchapp/SketchView.kt
@@ -0,0 +1,14 @@
+package com.just.sketchapp
+
+import android.content.Context
+import android.view.View
+import android.util.AttributeSet; //??
+
+
+
+
+class SketchView(context: Context, attr: AttributeSet?): View(context, attr) {
+
+
+
+}
